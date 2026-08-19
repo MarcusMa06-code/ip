4abb689e24a9ffdb1e864b1c2571d2c59a705ec6
Ma Mingyang <mingyang.ma@u.nus.edu>
2026-08-19 10:14:18 +0800

    Samantha.java: Echo user input until bye

--- src/main/java/Samantha.java
+++ src/main/java/Samantha.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
+
 public class Samantha {
+    private static final String LINE = "____________________________________________________________";
+
+    private static void printResponse(String content) {
+        System.out.println(LINE + "\n" + content + "\n" + LINE);
+    }
+
     public static void main(String[] args) {
         String banner = " ____    _    __  __    _    _   _ _____ _   _    _    \n"
                 + "/ ___|  / \\  |  \\/  |  / \\  | \\ | |_   _| | | |  / \\   \n"
                 + "\\___ \\ / _ \\ | |\\/| | / _ \\ |  \\| | | | | |_| | / _ \\  \n"
                 + " ___) / ___ \\| |  | |/ ___ \\| |\\  | | | |  _  |/ ___ \\ \n"
                 + "|____/_/   \\_\\_|  |_/_/   \\_\\_| \\_| |_| |_| |_/_/   \\_\\\n";
-        String welcomeMessage = "____________________________________________________________\n"
-                + banner
+
+        printResponse(banner
                 + "Hello! I'm Samantha.\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(welcomeMessage);
+                + "What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            printResponse(input);
+            input = scanner.nextLine();
+        }
+
+        printResponse("Bye. Let's talk next time!");
     }
 }
